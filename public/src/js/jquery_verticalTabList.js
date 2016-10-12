a3246444a8905d8a519aeb5d8a943a6600e41335
ystnk4 <ysztnk4@gmail.com>
2016-10-12 11:51:09 +0900

    refactor: 配列、変数を修正

--- public/src/js/jquery_verticalTabList.js
+++ public/src/js/jquery_verticalTabList.js
@@ -1,38 +1,39 @@
 (function($) {
-  'use strict';
+    'use strict';
 
-  $(function() {
+    $(function() {
 
-    // 選択中のカテゴリーからアイテムを削除する関数
-    function removeCheckedItem(itemId) {
-      $('[data-itemid=' + itemId + ']').remove();
-    }
+        // 選択中のカテゴリーからアイテムを削除する関数
+        function removeCheckedItem(itemId) {
+            $('[data-itemid=' + itemId + ']').remove();
+        }
 
-    // チェックボックスの処理
-    $('input').on('click', function() {
+        // チェックボックスの処理
+        $('input').on('click', function() {
 
-      var $thisItemId = $(this).attr('id');
-      var $categolyName = $('.nav .active').text();
-      var $itemName = $(this).val();
-      var $checkedItemTitle = new Array($categolyName, $itemName);
+            (new Array(100))
+            var thisItemId = $(this).attr('id'),
+                categolyName = $('.nav .active').text(),
+                itemName = $(this).val(),
+                checkedItemTitle = [categolyName, itemName];
 
-      if ($(this).prop('checked')) {
-        $('#checkedList').append('<div class="checkedList--item" data-itemid=' + $thisItemId + '><span>' + $checkedItemTitle + '</span><button class="">&times;</button></div>');
-      } else {
-        removeCheckedItem($thisItemId);
-      }
-    })
+            if ($(this).prop('checked')) {
+                $('#checkedList').append('<div class="checkedList--item" data-itemid=' + thisItemId + '><span>' + checkedItemTitle + '</span><button class="">&times;</button></div>');
+            } else {
+                removeCheckedItem(thisItemId);
+            }
+        })
 
-    // 削除ボタンの処理
-    $('#checkedList').on('click', 'button', function() {
+        // 削除ボタンの処理
+        $('#checkedList').on('click', 'button', function() {
 
-      var dataItemId = $(this).parents().data('itemid');
-      var metaItemId = '#' + dataItemId;
+            var dataItemId = $(this).parents().data('itemid'),
+                metaItemId = '#' + dataItemId;
 
-      removeCheckedItem(dataItemId);
-      $(metaItemId).prop('checked', false);
-    })
+            removeCheckedItem(dataItemId);
+            $(metaItemId).prop('checked', false);
+        })
 
-  });
+    });
 
 }).call(this, jQuery);
